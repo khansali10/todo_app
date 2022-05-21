92563a20de31bb13ad1b19b9cae8308e5a47e2d3
sachin khansali <84457317+khansali10@users.noreply.github.com>
2022-05-21 06:34:53 +0000

    checkbox click moving enabled

--- src/Completed.jsx
+++ src/Completed.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Completed(props) {
 
   return (
-    <div className="flex-row whitespace-pre-line bg-red-300 break-words w-4/5 ml-12">
+    <div className="flex-row whitespace-pre-line break-words w-4/5 ml-12">
       <input type="checkbox" checked className="w-4 h-4 accent-yellow-500 hover:ring hover:ring-slate-500 rounded-md" id={props.name} value={props.name} name={props.name} onChange={props.checkboxClick}></input>
       <label className="py-1  text-xl ml-2" htmlFor={props.name}>{props.name}</label>
     </div>
--- src/Incompleted.jsx
+++ src/Incompleted.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Incompleted(props){
     
   return (
-    <div className="flex-row items-center whitespace-pre-line  break-words bg-red-300 w-4/5 ml-12">
+    <div className="flex-row items-center whitespace-pre-line  break-words  w-4/5 ml-12">
       <input type="checkbox" id={props.name} className="w-4 h-4 
  hover:ring hover:ring-yellow-400  "  value={props.name} name={props.name} onChange={props.checkboxClick}></input>
       <label className="py-1 text-xl ml-2" htmlFor={props.name}>{props.name}</label>
